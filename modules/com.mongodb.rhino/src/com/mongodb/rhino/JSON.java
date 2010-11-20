bffd579e8089fd23b8bb03c4219039e8c097dc4f
Tal Liron <tal.liron@gmail.com>
2010-11-20 02:32:57 +0000

    Switch to Apache 2.0 license (to better match the official MongoDB drivers)
    git-svn-id: https://mongodb-rhino.googlecode.com/svn/trunk@29 4c41b6da-ce32-00c4-2e13-d615a01b235c

--- modules/com.mongodb.rhino/src/com/mongodb/rhino/JSON.java
+++ modules/com.mongodb.rhino/src/com/mongodb/rhino/JSON.java
@@ -1,9 +1,8 @@
 /**
  * Copyright 2010 Three Crickets LLC.
  * <p>
- * The contents of this file are subject to the terms of either the MPL version
- * 1.1 or the GPL version 2.0: http://www.opensource.org/licenses/mozilla1.1.php
- * http://www.opensource.org/licenses/gpl-license.html
+ * The contents of this file are subject to the terms of the Apache License
+ * version 2.0: http://www.opensource.org/licenses/apache2.0.php
  * <p>
  * Alternatively, you can obtain a royalty free commercial license with less
  * limitations, transferable or non-transferable, directly from Three Crickets
