54dfe5adc79d66c004696bc7b53f9106cdd41859
Tal Liron <tal.liron@gmail.com>
2011-12-15 20:30:31 -0600

    Fix Test.java for Rhino 1.7R3 Add Maven POM

--- modules/com.mongodb.rhino/src/com/mongodb/rhino/test/Test.java
+++ modules/com.mongodb.rhino/src/com/mongodb/rhino/test/Test.java
@@ -46,7 +46,7 @@ public class Test
 	// //////////////////////////////////////////////////////////////////////////
 	// Private
 
-	private static final String base = "importClass(java.lang.System, com.mongodb.rhino.BSON, com.mongodb.rhino.JSON); ";
+	private static final String base = "importClass(java.lang.System, com.mongodb.rhino.BSON);JSON = com.mongodb.rhino.JSON;";
 
 	private static void toJSON( String object )
 	{
