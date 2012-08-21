7fed70cacd36a5c6e608c9db9d4e7868d9cf25f2
Tal Liron <tal.liron@gmail.com>
2012-08-21 00:17:30 -0500

    Add copyright notice

--- components/mongodb-rhino/source/com/mongodb/rhino/MongoJsonImplementation.java
+++ components/mongodb-rhino/source/com/mongodb/rhino/MongoJsonImplementation.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2010-2012 Three Crickets LLC.
+ * <p>
+ * The contents of this file are subject to the terms of the Apache License
+ * version 2.0: http://www.opensource.org/licenses/apache2.0.php
+ * <p>
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly from Three Crickets
+ * at http://threecrickets.com/
+ */
+
 package com.mongodb.rhino;
 
 import com.threecrickets.rhino.JsonImplementation;
